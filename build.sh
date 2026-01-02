c6581d63c14a9f05cf508b04d65d436e2e52ea71
PedroAz <pdegutty@gmail.com>
2026-01-01 23:10:58 -0300

    forçando instalacao de dependencias no build

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# 1. Instalar as dependências
+# Tentamos primeiro o pip (padrão)
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# 2. Comandos do Django
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# 3. Script de criação de usuários
 python manage.py shell << END
 from django.contrib.auth import get_user_model
 from django.apps import apps
@@ -7,7 +21,6 @@ User = get_user_model()
 Account = apps.get_model('banco', 'Account')
 
 def criar_cliente(username, nome, cpf, email, senha):
-    # Verificamos se JÁ EXISTE alguém com esse username OU com esse CPF
     if not User.objects.filter(username=username).exists() and not User.objects.filter(cpf=cpf).exists():
         u = User.objects.create_user(
             username=username, 
@@ -18,15 +31,13 @@ def criar_cliente(username, nome, cpf, email, senha):
         )
         Account.objects.create(user=u, number=str(random.randint(100000, 999999)), account_type='corrente', balance=5000)
         Account.objects.create(user=u, number=str(random.randint(1000000, 9999999)), account_type='poupanca', balance=5000)
-        print(f"Usuario {username} criado com sucesso.")
+        print(f"Usuario {username} criado.")
     else:
-        print(f"Usuario {username} ou CPF {cpf} ja existem. Pulando criacao.")
+        print(f"Usuario {username} ou CPF {cpf} ja existem.")
 
-# Supervisor
 if not User.objects.filter(username='supervisor').exists():
     User.objects.create_superuser(username='supervisor', nome_completo='Admin', cpf='00000000000', email='a@a.com', password='admin_senha123')
 
-# Clientes
 criar_cliente('peter', 'Cliente Um', '29730565864', 'peter@gmail.com', 'senha123')
 criar_cliente('hook', 'Cliente Dois', '55566677788', 'hook@gmail.com', 'senha123')
 END
